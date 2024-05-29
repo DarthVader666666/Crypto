4b450f50658d2e24153bdc5da0da71666dc7ca21
Vadzim Rumiantsau <rumyancer@gmail.com>
2024-05-29 14:17:24 +0300

    Designed component event handlers

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,5 +1,12 @@
 <template>
-    <input type="number" placeholder="Enter number">
+    <ul>
+        <li>
+            <input min="0" type="number" @input="changeAmount($event.target.value)" placeholder="Enter number">
+        </li>
+        <li>
+            <button @click="convert()">Convert</button>    
+        </li>
+    </ul>
 </template>
 
 <script>
@@ -8,23 +15,43 @@ export default {
         changeAmount: {
             type: Function,
             required: true
+        },
+        convert: {
+            type: Function,
+            required: true
         }
     }
-
 }
 </script>
 
-<style>
+<style scoped>
     input {
+        position: relative;
         outline: none;
         width: 500px;
-        position: relative;
         top: -50px;
-        font-size: 1.25rem;
+        font-size: 1.25em;
         border-radius: 5px;
         border: 4px solid #1a032d;
         padding: 15px 20px;
         background: #fafafa;
         color: #333;
+    }
+
+    button {
+        position: relative;
+        top: -20px;
+        padding: 15px 20px;
+        color: #fff;
+        text-transform: uppercase;
+        cursor: pointer;
+        background: #1a032d;
+        border: 0;
+        border-radius: 3px;
+    }
+
+    ul {
+        list-style: none;
+        padding: 0;
     }
 </style>
